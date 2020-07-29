033e5c3a8ce890494948ad78851d0180dc268b40
Yaerhee <yaerhee@gmx.de>
2020-07-29 16:50:42 +0900

    map(), JSX element management, renderFood(), "key" prop, alt description, prop-types

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture, rating }) {
+  return (
+  <div>
+    <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
+  </div>
+  );
 }
 
 const foodILike = [
   {
-    name: 'Pasta'
+    id: 1,
+    name: 'Pasta',
     image: 'https://recipe1.ezmember.co.kr/cache/recipe/2018/12/14/2e5a56658f3abe62fa741b2958e3354e1.jpg',
+    rating: 5,
   },
   {
-    name: 'Samgyeopsal'
+    id: 2,
+    name: 'Samgyeopsal',
     image: 'https://cdn.crowdpic.net/list-thumb/thumb_l_AB0566EA909B44D840ACC8520191F53B.jpg',
+    rating: 4.9,
   },
 ];
+
 
 function App() {
   return (
     <div>
-      <h1>어서오세요.</h1>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 export default App;
