ffba9209de7c98f3a180a331722dc94ca0333d56
Yaerhee <yaerhee@gmx.de>
2020-08-06 09:56:24 +0900

    router and navigation settings

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <HashRouter>
-      <Route path="/" component={Home} />
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
   );
