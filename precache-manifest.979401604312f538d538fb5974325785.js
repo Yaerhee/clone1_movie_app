34fc2ed3cd79afa4b09c0188692317b9153ea391
Yaerhee <yaerhee@gmx.de>
2020-08-31 17:33:38 +0900

    Updates

---
+++ precache-manifest.979401604312f538d538fb5974325785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfc26c4b6a0397399103b492e2256e2",
+    "url": "/clone1_movie_app/index.html"
+  },
+  {
+    "revision": "7b513f63ad4ccbfec914",
+    "url": "/clone1_movie_app/static/css/main.bd6455a7.chunk.css"
+  },
+  {
+    "revision": "0f761466c4babe993c44",
+    "url": "/clone1_movie_app/static/js/2.3b3e71e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clone1_movie_app/static/js/2.3b3e71e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b513f63ad4ccbfec914",
+    "url": "/clone1_movie_app/static/js/main.2ee0bab7.chunk.js"
+  },
+  {
+    "revision": "88c93f0b291508f8b934",
+    "url": "/clone1_movie_app/static/js/runtime-main.d3530b11.js"
+  }
+]);
